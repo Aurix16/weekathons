44711a59f38019dd3b3996de7296160cb7ae4e87
aurix16 <dawn.idoko@gmail.com>
2020-07-20 21:00:00 -0500

    All 6 cups in place

--- Koolaid_Jar/script.js
+++ Koolaid_Jar/script.js
@@ -98,6 +98,78 @@ if (canvas && canvas.getContext) {
     context.stroke();
     context.restore();
 
+    //Rim of cup 3
+    context.save(); 
+    context.translate(translateWidth, translateHeight); 
+    context.scale(2,0.3); 
+    context.beginPath();
+    context.arc(312.5, 1030, 11.8, 0, Math.PI*2, false);
+    context.stroke(); 
+    context.restore();
+
+    //Body of cup 3
+    context.save();
+    context.translate(translateWidth, translateHeight);
+    context.beginPath();
+    context.moveTo(600, 310);
+    context.bezierCurveTo(550,430, 700,430, 650,310); //bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y)
+    context.stroke();
+    context.restore();
+
+    //Rim of cup 4
+    context.save(); 
+    context.translate(translateWidth, translateHeight); 
+    context.scale(2,0.3); 
+    context.beginPath();
+    context.arc(362.5, 1030, 11.8, 0, Math.PI*2, false);
+    context.stroke(); 
+    context.restore();
+
+    //Body of cup 4
+    context.save();
+    context.translate(translateWidth, translateHeight);
+    context.beginPath();
+    context.moveTo(700, 310);
+    context.bezierCurveTo(650,430, 800,430, 750,310); //bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y)
+    context.stroke();
+    context.restore();
+
+    //Rim of cup 5
+    context.save(); 
+    context.translate(translateWidth, translateHeight); 
+    context.scale(2,0.3); 
+    context.beginPath();
+    context.arc(412.5, 1030, 11.8, 0, Math.PI*2, false);
+    context.stroke(); 
+    context.restore();
+
+    //Body of cup 5
+    context.save();
+    context.translate(translateWidth, translateHeight);
+    context.beginPath();
+    context.moveTo(800, 310);
+    context.bezierCurveTo(750,430, 900,430, 850,310); //bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y)
+    context.stroke();
+    context.restore();
+
+    //Rim of cup 6
+    context.save(); 
+    context.translate(translateWidth, translateHeight); 
+    context.scale(2,0.3); 
+    context.beginPath();
+    context.arc(462.5, 1030, 11.8, 0, Math.PI*2, false);
+    context.stroke(); 
+    context.restore();
+
+    //Body of cup 6
+    context.save();
+    context.translate(translateWidth, translateHeight);
+    context.beginPath();
+    context.moveTo(900, 310);
+    context.bezierCurveTo(850,430, 1000,430, 950,310); //bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y)
+    context.stroke();
+    context.restore();
+
 
     
 
